162fed28043c7d2794eeb50d0414e28b6957b177
Oliver Vazquez <olivervazquez@gmail.com>
2021-06-17 19:12:08 -0400

    add and remove tasks

--- server.js
+++ server.js
@@ -9,25 +9,39 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-var tasks = [
-  {
-    name: "Get groceries",
-    date: "August 7th",
-    description: "get groceries from the store",
-    ID: 0,
-  },
-  {
-    name: "Go to class",
-    date: "August 8th",
-    description: "Go to class you bum",
-    ID: 1,
-  },
-];
+var tasks = [];
+
+var ID = 2;
 
 app.get("/tasks", (req, res) => {
   res.send({ tasks: tasks });
 });
 
-app.listen(port, () =>
-  console.log(`Hello world app listening on port ${port}!`)
-);
+app.post("/addtask", (req, res) => {
+  var body = req.body;
+  var task = {
+    name: body.task.name,
+    date: body.task.date,
+    description: body.task.description,
+    ID: ID,
+  };
+  tasks.push(task);
+  ++ID;
+  res.send("Success");
+});
+
+app.delete("/:ID", (req, res) => {
+  const ID = parseInt(req.params.ID);
+  var newTasks = [];
+  console.log(ID);
+  console.log(tasks);
+  for (var i = 0; i < tasks.length; ++i) {
+    if (tasks[i].ID != ID) {
+      newTasks.push(tasks[i]);
+    }
+  }
+  tasks = newTasks;
+  res.send("Success");
+});
+
+app.listen(port);
