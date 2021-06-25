e9187253556fd445269f7f89dd1aa2f13dbf9ce5
Oliver Vazquez <olivervazquez@gmail.com>
2021-06-25 17:44:53 -0400

    Fixed port var

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const db = mysql.createPool({
   host: "localhost",
